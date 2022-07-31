b00dfec9842dcf1e976177606676da24d7f3d0f8
Babatunde Owolabi <chatwittundeowolabi@gmail.com>
2022-07-31 08:01:34 +0100

    now validate mtn,airtel, etisalat and glo numbers

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 function phoneNumber(input){
-    var phone = /^\+234 (703|706|803|806|810|813|814|816|906|906|913|916|704) \d{3} \d{4}$/;
-    if(input.value.match(phone))
+    var mtnPattern = /^\+234 (703|706|803|806|810|813|814|816|906|906|913|916|704) \d{3} \d{4}$/;
+    var airtelPattern = /^\+234 (701|708|802|808|812|901|902|904|907|912) \d{3} \d{4}$/;
+    var etisalatPattern = /^\+234 (809|817|818|909|908) \d{3} \d{4}$/;
+    var gloPattern = /^\+234 (705|805|807|811|815|905|915) \d{3} \d{4}$/;
+
+
+    if((input.value.match(mtnPattern)) && !(input.value.match(airtelPattern)) && !(input.value.match(etisalatPattern)) && !(input.value.match(gloPattern)))
     {
         alert("mtn number");
         return true;
     }
-    else{
-        alert("Invalid number");
+    else if(!(input.value.match(mtnPattern)) && (input.value.match(airtelPattern)) && !(input.value.match(etisalatPattern)) && !(input.value.match(gloPattern)))
+    {
+        alert("airtel number");
+        return true;
+    }
+    else if(!(input.value.match(mtnPattern)) && !(input.value.match(airtelPattern)) && (input.value.match(etisalatPattern)) && !(input.value.match(gloPattern)))
+    {
+        alert("etisalat number");
+        return true;
+    }
+    else if(!(input.value.match(mtnPattern)) && !(input.value.match(airtelPattern)) && !(input.value.match(etisalatPattern)) && (input.value.match(gloPattern)))
+    {
+        alert("glo number");
+        return true;
+    }
+    else
+    {
+        alert("invalid number pattern");
         return false;
     }
 }
