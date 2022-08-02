1dc566559ad1447ced938f37623fdb3cbdbcc916
chatwittundeowolabi@gmail.com <chatwittundeowolabi@gmail.com>
2022-08-02 09:32:26 +0100

    removed compulsory space

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function phoneNumber(input){
-    var mtnPattern = /^\+234 (703|706|803|806|810|813|814|816|906|906|913|916|704) \d{3} \d{4}$/;
-    var airtelPattern = /^\+234 (701|708|802|808|812|901|902|904|907|912) \d{3} \d{4}$/;
-    var etisalatPattern = /^\+234 (809|817|818|909|908) \d{3} \d{4}$/;
-    var gloPattern = /^\+234 (705|805|807|811|815|905|915) \d{3} \d{4}$/;
+    var mtnPattern = /^\+234(703|706|803|806|810|813|814|816|906|906|913|916|704)\d{7}$/;
+    var airtelPattern = /^\+234(701|708|802|808|812|901|902|904|907|912)\d{7}$/;
+    var etisalatPattern = /^\+234(809|817|818|909|908)\d{7}$/;
+    var gloPattern = /^\+234(705|805|807|811|815|905|915)\d{7}$/;
 
 
     if((input.value.match(mtnPattern)) && !(input.value.match(airtelPattern)) && !(input.value.match(etisalatPattern)) && !(input.value.match(gloPattern)))
